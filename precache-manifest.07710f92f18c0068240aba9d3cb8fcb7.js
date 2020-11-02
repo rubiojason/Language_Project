dba2edd73de18403b957a3e29503dd165aad6f3b
Jason Rubio <jason@Denises-iMac.local>
2020-11-01 20:53:27 -0700

    Updates

---
+++ precache-manifest.07710f92f18c0068240aba9d3cb8fcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37102255a7d05e008325f830b4196611",
+    "url": "/Language-Project/index.html"
+  },
+  {
+    "revision": "814f1d3d04ae2a97ddba",
+    "url": "/Language-Project/static/css/main.ee1942d7.chunk.css"
+  },
+  {
+    "revision": "70811b1f0b52fec0b279",
+    "url": "/Language-Project/static/js/2.783ea629.chunk.js"
+  },
+  {
+    "revision": "c84b58114f3b1bf80d007b8fb41efb93",
+    "url": "/Language-Project/static/js/2.783ea629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814f1d3d04ae2a97ddba",
+    "url": "/Language-Project/static/js/main.b4ea86a6.chunk.js"
+  },
+  {
+    "revision": "ecb2375e7c40ce919951",
+    "url": "/Language-Project/static/js/runtime-main.6ff3897e.js"
+  }
+]);
